a51d8f1d5006780a5e2eabe8c861efc2156c9370
dklarin <danijel.klarin@gmail.com>
2021-11-08 23:45:49 +0100

    list comprehension (dev_list)

--- dev_scraps.py
+++ dev_scraps.py
@@ -1,30 +1,27 @@
 import scrapy
 import json
 
+
 class DevtosSpider(scrapy.Spider):
 
     url_list = []
     page = 'https://dev.to/search/feed_content?per_page=100&page='
     for i in range(0, 10):
         url_list.append(page+str(i))
-    
+
     name = "devto"
     start_urls = url_list
-    
+
     def parse(self, response):
-        dev_list = []
         data = json.loads(response.text)
 
-        for i in data['result']:
-            dev_list.append(i)
+        dev_list = [i for i in data['result']]
 
-        for i in range(len(dev_list)):            
+        for i in range(len(dev_list)):
             yield {
                 'title': dev_list[i]['title'],
                 'author': dev_list[i]['user']['name'],
                 'tag_list': dev_list[i]['tag_list'],
                 'date': dev_list[i]['readable_publish_date'],
                 'reading_time': dev_list[i]['reading_time']
-            }   
-        
- 
+            }
